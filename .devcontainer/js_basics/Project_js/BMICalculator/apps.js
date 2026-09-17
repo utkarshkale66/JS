5344001cfbc23bbb37a026b04e25c344360de397
Utkarsh Kale <ukale8443@gmail.com>
2026-09-17 18:09:46 +0000

    Project 2 done

--- .devcontainer/js_basics/Project_js/BMICalculator/apps.js
+++ .devcontainer/js_basics/Project_js/BMICalculator/apps.js
@@ -1,12 +1,12 @@
-const form= document.querySelector('form')
+const form= document.querySelector('form');
 
-form.addEventListener('submit', function(e) {
-e.preventDefault()
+form.addEventListener('submit', function (e) {
+e.preventDefault();
 
 
 const height= parseInt(document.querySelector('#height').value);
 const weight= parseInt(document.querySelector('#weight').value);
-const results=document.querySelector('#results');
+const results =document.querySelector('#results');
 
 if (height === '' || height < 0 || isNaN(height)) {
     results.innerHTML = 'Please give a valid height ${height}';
@@ -14,7 +14,7 @@ if (height === '' || height < 0 || isNaN(height)) {
     results.innerHTML= 'Please give a valid weight ${weight}';
 } else {
     const bmi = (weight /  ((height * height) /10000)).toFixed(2);
-    results.innerHTML = '<span>${bmi}</span>';
+   results.innerHTML = `<span>${bmi}</span>`;
 }
 });
 
